bdc5bb5d400ebf2d34f6955ba27e00dcfe5d9c27
skowronkow <skowronkow@gmail.com>
2014-10-13 02:11:22 +0200

    working on example page

--- test/main.js
+++ test/main.js
@@ -18,9 +18,8 @@ require(['domReady', 'jtop'], function(domReady, jtop) {
         // background
         // -------------------------------------------------------------------------------------------------------------
 
-        var t = new Trianglify();
-
         function geneerateBackground() {
+            var t = new Trianglify();
             var pattern = t.generate(document.body.clientWidth, document.body.clientHeight);
             document.body.setAttribute('style', 'background-image: ' + pattern.dataUrl);
         }
